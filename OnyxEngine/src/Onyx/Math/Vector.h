6ce64f76c477e0eed93198e40557ffed465a9c29
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-01-23 14:50:55 +0200

    Combined Vector2 and Vector3 into Onyx::Vector, added new overload for vector /, made some Vector methods to use reference

--- OnyxEngine/src/Onyx/Math/Vector.h
+++ OnyxEngine/src/Onyx/Math/Vector.h
@@ -2,49 +2,25 @@
 
 namespace Onyx
 {
-	// 2 component vector. Suitable for 2D games
-	// Z is constant and set to 0.0
-	struct ONYX_API Vector2
+	// 3D Vector of X, Y and Z values
+	struct ONYX_API Vector
 	{
-		Vector2(float x, float y) :
-			X(x), Y(y) { }
-
-		float operator*(Vector2 v1) { return (X * v1.X) + (Y * v1.Y); }
-		Vector2 operator+(Vector2 v1) { return { X + v1.X, Y + v1.Y }; }
-		Vector2 operator-(Vector2 v1) { return { X - v1.X, Y - v1.Y }; }
-		Vector2 operator/(float value) { return { X / value, Y / value }; }
-
-		// Add another Vector2
-		// Overwrites the values in vector
-		inline void Add(Vector2 v1) { X += v1.X; Y += v1.Y; }
-
-		float Length() { return sqrt((*this) * (*this)); }
-
-		inline Vector2 Normalize(float value) { return (*this) / Length(); }
-
-		float X, Y;
-		const float Z = 0.0f;
-	};
-
-		// 3 component vector
-		// Suitable for 2.5D and 3D games
-	struct ONYX_API Vector3
-	{
-		Vector3(float x, float y, float z) :
+		Vector(float x, float y, float z) :
 			X(x), Y(y), Z(z) { }
-		Vector3 operator+(Vector3 v1) { return { X + v1.X, Y + v1.Y, Z + v1.Z }; }
-		Vector3 operator-(Vector3 v1) { return { X - v1.X, Y - v1.Y, Z - v1.Z }; }
+		inline Vector operator+ (Vector& v1) { return { X + v1.X, Y + v1.Y, Z + v1.Z }; }
+		inline Vector operator- (Vector& v1) { return { X - v1.X, Y - v1.Y, Z - v1.Z }; }
 		// Returns a dot product of 2 vectors
-		float operator*(Vector3 v1) { return(X * v1.X) + (Y * v1.Y) + (Z * v1.Z); }
-		Vector3 operator/(float value) { return { X / value, Y / value, Z / value }; }
+		inline float operator* (Vector& v1) { return(X * v1.X) + (Y * v1.Y) + (Z * v1.Z); }
+		inline Vector operator/ (float value) { return { X / value, Y / value, Z / value }; }
+		inline Vector operator/ (Vector& v1) { return { X / v1.X, Y / v1.Y, Z / v1.Z }; }
 
 		// Add another Vector3
 		// Overwrites the values in Vector
-		inline void Add(Vector3 v1) { X += v1.X; Y += v1.Y; Z += v1.Z; }
+		inline void Add(Vector v1) { X += v1.X; Y += v1.Y; Z += v1.Z; }
 
-		float Length() { return sqrt((*this) * (*this)); }
+		inline float Length() { return sqrt((*this) * (*this)); }
 
-		inline Vector3 Normalize(float value) { return (*this) / Length(); }
+		inline Vector Normalize(float value) { return (*this) / Length(); }
 
 		float X, Y, Z;
 	};
